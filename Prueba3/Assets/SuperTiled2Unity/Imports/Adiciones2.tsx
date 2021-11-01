049d636484dc15b0a9759a8b59d4e0f62f402f13
Mycaelai <walejandropg15@gmail.com>
2021-11-01 13:43:38 -0500

    Adicion proyecto con mapa

---
+++ Prueba3/Assets/SuperTiled2Unity/Imports/Adiciones2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Adiciones2" tilewidth="16" tileheight="16" tilecount="217" columns="31">
+ <image source="Templete2.png" width="496" height="112"/>
+</tileset>
