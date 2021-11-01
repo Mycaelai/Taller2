829b8183f760baf5bbbd4d4eb030af9594eabe15
Mycaelai <walejandropg15@gmail.com>
2021-11-01 18:37:02 -0500

    Mapas anexados

---
+++ Prueba3/Assets/SuperTiled2Unity/Imports/Cosas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="Cosas" tilewidth="16" tileheight="16" tilecount="224" columns="32">
+ <image source="TempleteParcial.png" width="512" height="116"/>
+</tileset>
