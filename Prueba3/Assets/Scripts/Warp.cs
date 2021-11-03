db37393835f1357bbc409743d3031e5e5adb7140
Mycaelai <walejandropg15@gmail.com>
2021-11-02 23:11:35 -0500

    Transicion suave

--- Prueba3/Assets/Scripts/Warp.cs
+++ Prueba3/Assets/Scripts/Warp.cs
@@ -8,6 +8,11 @@ public class Warp : MonoBehaviour
     public GameObject target;
 
     public GameObject targetMap;
+
+    bool start = false;
+    bool isFadeIn = false;
+    float alpha = 0;
+    float fadeTime = 1f;
     // Start is called before the first frame update
 
     void Awake()
@@ -20,10 +25,51 @@ public class Warp : MonoBehaviour
         Assert.IsNotNull(targetMap);
     }
 
-    void OnTriggerEnter2D(Collider2D col)
+    IEnumerator OnTriggerEnter2D(Collider2D col)
     {
+        FadeIn();
+        yield return new WaitForSeconds(fadeTime);
+
         col.transform.position = target.transform.GetChild(0).transform.position;
 
         //Camera.main.GetComponent<MainCamera>().SetBound(targetMap);
+
+        FadeOut();
+    }
+
+    void OnGUI()
+    {
+        if (!start)
+            return;
+
+        GUI.color = new Color(GUI.color.r, GUI.color.g, GUI.color.b, alpha);
+
+        Texture2D tex;
+        tex = new Texture2D(1, 1);
+        tex.SetPixel(0, 0, Color.black);
+        tex.Apply();
+
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), tex);
+
+        if (isFadeIn)
+        {
+            alpha = Mathf.Lerp(alpha, 1.1f, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            alpha = Mathf.Lerp(alpha, -0.1f, fadeTime * Time.deltaTime);
+
+            if (alpha < 0) start = false;
+        }
+    }
+
+    void FadeIn()
+    {
+        start = true;
+        isFadeIn = true;
+    }
+    void FadeOut()
+    {
+        isFadeIn = false;
     }
 }
